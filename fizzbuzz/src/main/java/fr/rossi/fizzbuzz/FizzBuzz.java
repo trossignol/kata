8eafa4ca7bac4774f5b3414f74c80245b51bc370
trossignol <thomas.rossignol.pro@gmail.com>
2022-07-23 10:58:02 +0000

    Add main

--- fizzbuzz/src/main/java/fr/rossi/fizzbuzz/FizzBuzz.java
+++ fizzbuzz/src/main/java/fr/rossi/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,7 @@ package fr.rossi.fizzbuzz;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -9,6 +10,12 @@ public class FizzBuzz {
     private static List<Rule> SORTED_RULES = List.of(
             new Rule(3, "Fizz"),
             new Rule(5, "Buzz"));
+
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 100)
+                .mapToObj(FizzBuzz::fizzbuzz)
+                .forEach(System.out::println);
+    }
 
     static String fizzbuzz(int value) {
         final String result = SORTED_RULES.stream()
